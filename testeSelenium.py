a4ec18ff7349c59c50b2b69a9da9075eb210bb99
Vinicius de Lima Gomes <37003005+vinnilmg@users.noreply.github.com>
2019-04-26 17:20:12 -0300

    Update testeSelenium.py

--- testeSelenium.py
+++ testeSelenium.py
@@ -2,16 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 chrome = webdriver.Chrome()
-chrome.get('http://saude.sulamerica.br/ressarcimentosus/public/login.jsf')
+chrome.get('http:/')
 
 campo_busca = chrome.find_element_by_name('usuario')
-campo_busca.send_keys('susis')
+campo_busca.send_keys('')
 
 campo_busca = chrome.find_element_by_name('senha')
-campo_busca.send_keys('sas155')
+campo_busca.send_keys('')
 
 #campo_busca.send_keys(Keys.ENTER) #esse aperta enter
-botao = chrome.find_element_by_id('loginBtn')
+botao = chrome.find_element_by_id('')
 botao.click()
 
 
