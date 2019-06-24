5145c27326ac4d144221621b99f27948adc8d766
Vinicius de Lima Gomes <37003005+vinnilmg@users.noreply.github.com>
2019-06-24 10:03:58 -0300

    Add files via upload

---
+++ boxplot.py
@@ -0,0 +1,13 @@
+#Boxplot - diagrama de caixa
+import matplotlib.pyplot as plt
+import random
+
+vetor = []
+
+for i in range(10):
+	numero_aleatorio = random.randint(0,5)
+	vetor.append(numero_aleatorio)
+
+plt.boxplot(vetor)
+plt.title("Meu Boxplot")
+plt.show()
